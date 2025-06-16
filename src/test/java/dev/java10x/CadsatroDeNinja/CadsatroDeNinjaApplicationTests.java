e165c4eaaef3d8ae805b7690801d0c6d0f69203c
Eric Silva <70177571+Eriskgb@users.noreply.github.com>
2025-06-16 17:53:08 -0300

    first commit

---
+++ src/test/java/dev/java10x/CadsatroDeNinja/CadsatroDeNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadsatroDeNinja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadsatroDeNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
