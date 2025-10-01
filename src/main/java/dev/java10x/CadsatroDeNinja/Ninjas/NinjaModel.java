0932fa9f9115b2a7688f8017db1fbf6fc808a5a2
Eric Silva <70177571+Eriskgb@users.noreply.github.com>
2025-09-30 23:13:45 -0300

    feat: adiciona migration para criar tabela tb_aldeia

--- src/main/java/dev/java10x/CadsatroDeNinja/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadsatroDeNinja/Ninjas/NinjaModel.java
@@ -1,4 +1,5 @@
 package dev.java10x.CadsatroDeNinja.Ninjas;
+import dev.java10x.CadsatroDeNinja.Aldeia.AldeiaModel;
 import dev.java10x.CadsatroDeNinja.Biju.BijuModel;
 import dev.java10x.CadsatroDeNinja.Missoes.MissoesModel;
 import jakarta.persistence.*;
@@ -30,5 +31,9 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "jinchuriki_id")
     private BijuModel jinchuriki;
+
+    @ManyToOne
+    @JoinColumn(name = "aldeia_id")
+    private AldeiaModel aldeia;
 
 }
